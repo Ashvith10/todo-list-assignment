9d9ff1a30b274ab2234df1122da42c34c20cc42b
Ashvith Shetty <Ashvith@noreply.codeberg.org>
2024-10-29 15:49:14 +0530

    feat: Initialize database on startup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from db import init_db
 from routers.v1 import todos
 
 app = FastAPI()
 app.include_router(todos.router, prefix="/api/v1")
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
