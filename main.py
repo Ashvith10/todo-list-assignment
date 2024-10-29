9ae7a3967be2cd101c50df021e10601b1df9f10c
Ashvith Shetty <Ashvith@noreply.codeberg.org>
2024-10-29 12:36:21 +0530

    refactor: Add versioning to namespace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import todos
+from routers.v1 import todos
 
 app = FastAPI()
-app.include_router(todos.router)
+app.include_router(todos.router, prefix="/api/v1")
