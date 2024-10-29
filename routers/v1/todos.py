c3bceb42da5620da63ac306bea84940274384f4c
Ashvith Shetty <Ashvith@noreply.codeberg.org>
2024-10-29 16:58:40 +0530

    chore: Add details about endpoints
    - Fill information about endpoints - summary, description, status code
    and response model

--- routers/v1/todos.py
+++ routers/v1/todos.py
@@ -5,13 +5,25 @@ from sqlmodel import select
 
 router = APIRouter(prefix="/todos")
 
-@router.get("/", response_model=list[TodoOutput])
+@router.get(
+    "/",
+    response_model=list[TodoOutput],
+    summary="Get all the todo lists.",
+    description="Get all the todo lists. Returns a list of objects with the following fields: `description`, `id`, `title` and `completed`.",
+    status_code=200
+)
 async def read_todos(session: SessionDep):
     todos = session.exec(select(Todo)).all()
 
     return todos
 
-@router.post("/", response_model=TodoOutput)
+@router.post(
+    "/",
+    response_model=TodoOutput,
+    summary="Create a new todo list.",
+    description="Create a new todo list. Returns a object with the following fields: `description`, `id`, `title` and `completed`.",
+    status_code=201
+)
 async def create_todo(todo: TodoInput, session: SessionDep):
     todo = Todo.model_validate(todo)
 
@@ -21,7 +33,13 @@ async def create_todo(todo: TodoInput, session: SessionDep):
 
     return todo
 
-@router.get("/{id}", response_model=TodoOutput)
+@router.get(
+    "/{id}",
+    response_model=TodoOutput,
+    summary="Get a todo list.",
+    description="Get a todo list, based on the `id` parameter passed. Returns a object with the following fields: `description`, `id`, `title` and `completed`.",
+    status_code=200
+)
 async def read_todo(id: int, session: SessionDep):
     todo = session.get(Todo, id)
 
@@ -30,7 +48,13 @@ async def read_todo(id: int, session: SessionDep):
 
     return todo
 
-@router.put("/{id}")
+@router.put(
+    "/{id}",
+    response_model=TodoOutput,
+    summary="Update a existing todo list.",
+    description="Update a existing todo list. Returns a object with the following fields: `description`, `id`, `title` and `completed`.",
+    status_code=200
+)
 async def update_todo(id: int, todo_patch: TodoInput, session: SessionDep):
     todo = session.get(Todo, id)
 
@@ -46,7 +70,13 @@ async def update_todo(id: int, todo_patch: TodoInput, session: SessionDep):
 
     return todo
 
-@router.delete("/{id}", status_code=204)
+@router.delete(
+    "/{id}",
+    response_model=None,
+    summary="Delete a existing todo list.",
+    description="Delete a existing todo list. Returns nothing.",
+    status_code=204
+)
 async def delete_todo(id: int, session: SessionDep):
     todo = session.get(Todo, id)
 
