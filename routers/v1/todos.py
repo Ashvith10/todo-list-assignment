aade17df5d23e7e485ecd43eed9215c4be1c2f9b
Ashvith Shetty <Ashvith@noreply.codeberg.org>
2024-10-29 12:37:18 +0530

    feat: Add the complete REST API the endpoints for `todos`

--- routers/v1/todos.py
+++ routers/v1/todos.py
@@ -3,5 +3,21 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/todos")
 
 @router.get("/")
-async def get_root():
-    return {"msg": "Get all todos"}
+async def read_todos():
+    return {"msg": "Get all todos."}
+
+@router.post("/")
+async def create_todo():
+    return {"msg": "Create a todo."}
+
+@router.get("/{id}")
+async def read_todo(id: int):
+    return {"msg": f"Get todo at id {id}."}
+
+@router.put("/{id}")
+async def update_todo(id: int):
+    return {"msg": f"Update todo at id {id}."}
+
+@router.delete("/{id}")
+async def delete_todo(id: int):
+    return {"msg": f"Delete todo at {id}."}
