429808a52f858ebea6dfa2f1df89ba14a1722fab
Ashvith Shetty <Ashvith@noreply.codeberg.org>
2024-10-29 16:57:53 +0530

    chore: Add title and description, disable Redoc

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from db import init_db
 from routers.v1 import todos
 
-app = FastAPI()
+app = FastAPI(
+    title="todo-list-application",
+    summary="A take-home assignment for `Bugs and Glitches`.",
+    redoc_url=None
+)
 app.include_router(todos.router, prefix="/api/v1")
 
 @app.on_event("startup")
